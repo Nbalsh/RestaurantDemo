475c3a960816b3ee15e6b2f9a7b32afce647b0ad
Nbalsh <brandon.bn.nelson@gmail.com>
2015-09-20 18:25:14 -0700

    Basic set up for DB
    Started to do DB set up. Needs more functionality

---
+++ app/src/main/java/com/example/nodnarb/restaurantdemo/FeedReaderContract.java
@@ -0,0 +1,44 @@
+package com.example.nodnarb.restaurantdemo;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Nodnarb on 2015-09-20.
+ */
+public final class FeedReaderContract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    public FeedReaderContract() {}
+
+    /* Inner class that defines the table contents */
+    public static abstract class RestaurantEntry implements BaseColumns {
+        public static final String TABLE_NAME = "restaurant";
+        public static final String COLUMN_NAME_ENTRY_ID = "id";
+        public static final String COLUMN_NAME_TITLE = "rName";
+        public static final String COLUMN_NAME_LOCATION = "rLocation";
+    }
+
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String COMMA_SEP = ",";
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + RestaurantEntry.TABLE_NAME + " (" +
+                    RestaurantEntry._ID + " INTEGER PRIMARY KEY," +
+                    RestaurantEntry.COLUMN_NAME_ENTRY_ID + TEXT_TYPE + COMMA_SEP +
+                    RestaurantEntry.COLUMN_NAME_TITLE + TEXT_TYPE + COMMA_SEP +
+                    RestaurantEntry.COLUMN_NAME_LOCATION + TEXT_TYPE +
+
+    // Any other options for the CREATE command
+            " )";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + RestaurantEntry.TABLE_NAME;
+
+    public String getSqlCreateEntries() {
+        return SQL_CREATE_ENTRIES;
+    }
+    public String getSqlDeleteEntries() {
+        return SQL_DELETE_ENTRIES;
+    }
+
+}
+
---
+++ app/src/main/java/com/example/nodnarb/restaurantdemo/RestaurantReaderDbHelper.java
@@ -0,0 +1,33 @@
+package com.example.nodnarb.restaurantdemo;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Nodnarb on 2015-09-20.
+ */
+// DB HELPER
+public class RestaurantReaderDbHelper extends SQLiteOpenHelper {
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "RestaurantReader.db";
+    FeedReaderContract readerContract = new FeedReaderContract();
+
+    public RestaurantReaderDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(readerContract.getSqlCreateEntries());
+    }
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(readerContract.getSqlDeleteEntries());
+        onCreate(db);
+    }
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+
+}

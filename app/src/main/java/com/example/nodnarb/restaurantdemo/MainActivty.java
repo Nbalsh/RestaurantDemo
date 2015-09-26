b94f38a957e449bf7a3b9ad1c6b4162374ad942a
Jasdeep Kahlon <jasdeep.kln@gmail.com>
2015-09-25 21:03:12 -0700

    onClick brings up list item position, refactoring

--- app/src/main/java/com/example/nodnarb/restaurantdemo/MainActivty.java
+++ app/src/main/java/com/example/nodnarb/restaurantdemo/MainActivty.java
@@ -1,35 +1,43 @@
 package com.example.nodnarb.restaurantdemo;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 import android.app.Activity;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivty extends Activity {
 
   private ListView mainListView ;
   private ArrayAdapter<String> listAdapter ;
-    RestaurantReaderDbHelper mDbHelper;
+  private RestaurantReaderDbHelper mDbHelper;
+
   /** Called when the activity is first created. */
   @Override
-  public void onCreate(Bundle savedInstanceState) {
+  public void onCreate(Bundle savedInstanceState)
+  {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main_activty);
 
       InsertData();
 
-// Set the ArrayAdapter as the ListView's adapter.
-    mainListView.setAdapter( listAdapter );
+    // Set the ArrayAdapter as the ListView's adapter.
+      mainListView.setAdapter(listAdapter);
+
+    // React to user clicks on item
+      SetOnClickListener();
   }
 
-    private void InsertData() {
+    private void InsertData()
+    {
         // Find the ListView resource.
         mainListView = (ListView) findViewById( R.id.mainListView );
 
@@ -42,7 +50,8 @@ public class MainActivty extends Activity {
         listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, restaurantNames);
     }
 
-    private String[] GetDataFromDB() {
+    private String[] GetDataFromDB()
+    {
 //        setContentView(R.layout.activity_main_activty);
         mDbHelper = new RestaurantReaderDbHelper(getApplicationContext());
 
@@ -62,14 +71,9 @@ public class MainActivty extends Activity {
 
         // Create a new map of values, where column names are the keys
         ContentValues values = new ContentValues();
-        values.put(RestaurantReaderContract.RestaurantEntry.COLUMN_NAME_ENTRY_ID, id);
-        values.put(RestaurantReaderContract.RestaurantEntry.COLUMN_NAME_TITLE, title);
-        values.put(RestaurantReaderContract.RestaurantEntry.COLUMN_NAME_LOCATION, location);
-
+        values = PutValues(id, title, location, values);
         id++;
-        values.put(RestaurantReaderContract.RestaurantEntry.COLUMN_NAME_ENTRY_ID, id);
-        values.put(RestaurantReaderContract.RestaurantEntry.COLUMN_NAME_TITLE, anotherTitle);
-        values.put(RestaurantReaderContract.RestaurantEntry.COLUMN_NAME_LOCATION, anotherLocation);
+        values = PutValues(id, anotherTitle, anotherLocation, values);
 
         // Insert the new row, returning the primary key value of the new row
         long newRowId;
@@ -100,12 +104,38 @@ public class MainActivty extends Activity {
         int columnIndex = c.getColumnIndex(RestaurantReaderContract.RestaurantEntry.COLUMN_NAME_TITLE);
         String[] restaurantNames = new String[c.getCount()];
         int j = 0;
-        for (c.moveToFirst(); j < c.getCount(); c.moveToNext()){
+        for (c.moveToFirst(); j < c.getCount(); c.moveToNext())
+        {
             restaurantNames[j] = c.getString(columnIndex);
             j++;
         }
         c.close();
         
         return restaurantNames;
+    }
+
+    private ContentValues PutValues(int id, String title, String location, ContentValues values)
+    {
+        values.put(RestaurantReaderContract.RestaurantEntry.COLUMN_NAME_ENTRY_ID, id);
+        values.put(RestaurantReaderContract.RestaurantEntry.COLUMN_NAME_TITLE, title);
+        values.put(RestaurantReaderContract.RestaurantEntry.COLUMN_NAME_LOCATION, location);
+        return values;
+    }
+
+
+
+    private void SetOnClickListener()
+    {
+        mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            public void onItemClick(AdapterView<?> parentAdapter, View view, int position,
+                                    long id)
+            {
+                // We know the View is a TextView so we can cast it
+                TextView clickedView = (TextView) view;
+
+                Toast.makeText(MainActivty.this, "Item with id [" + id + "] - Position [" + position + "] - Restaurant [" + clickedView.getText() + "]", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
